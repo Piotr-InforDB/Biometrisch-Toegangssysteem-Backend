3a8c231a8b74962e7b2f6e2db3b93e1aeb9559d7
Piotr <piotrek124@hotmail.com>
2025-04-14 17:35:00 +0200

    Dockerized imate test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Dockerized NestJS is working!';
+    return 'Dockerized Nest.JS is working!';
   }
 }
